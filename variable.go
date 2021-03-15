cb041b7d538c8b52ec41003f8cec2f60a3b02a0e
liulinboyi <814921718@qq.com>
2021-03-15 21:47:19 +0800

    feat: Add variable

---
+++ variable.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+	var a int     // 如果没有赋值，默认为0
+	var b int = 1 // 声明时赋值
+	var c = 1     // 声明时赋值
+
+	d := 1
+	msg := "Hello World!"
+
+	println(a, b, c, d, msg)
+
+	var e int8 = 10
+	var c1 byte = 'a'
+	var f float32 = 12.2
+	var msg1 = "Hello World"
+	ok := false
+
+	println(e, c1, f, msg1, ok)
+
+}
